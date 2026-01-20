c9b6b5cf3c903105ad8184c0c4108dd63f965c8d
BrunoAlves-Web <108080978+BrunoYakuza@users.noreply.github.com>
2026-01-19 22:25:26 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+a = float(input("Digite um número: "))
+b = float(input("Digite outro número: "))
+
+print("Soma:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
